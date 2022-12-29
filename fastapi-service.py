d14d2a48a00405f9ff8b3aef2f9fd2164d6d90d9
Ayodele Seriki <seriki.ayodele@deimos.co.za>
2022-12-29 14:50:41 +0100

    i created a repository

---
+++ fastapi-service.py
@@ -0,0 +1,12 @@
+# create a fastapi service with python that reads and writes data from redis cache.........if someone post 
+# data to an endpoint we want to take that data an store it in a redis cache, 
+# if someone hits the endpoint with a get request we want to return data from redis cache, 
+# create metrics to measure the performance of the service....how long it takes the service to 
+# read and write from the cache(how fast), how long each request takes
+
+# export grafana metrics using prometheus
+
+# THINGS TO LEARN
+# ===============
+# -read and write data from redis cache
+# -api's and endpointsmetrics with grafana 
